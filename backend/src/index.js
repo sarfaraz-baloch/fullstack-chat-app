b8ce0f6941b3da8a93451d67c4cd83191f7ffad2
sarfaraz-baloch <sarfarazkohda321@gmail.com>
2024-11-24 02:40:48 +0500

    fixing bugs

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,3 @@
-// 
-
 import express from "express"; // import express
 import dotenv from "dotenv"; // import dotenv for environment variables
 import authRoutes from "./routes/auth.route.js";
@@ -22,17 +20,17 @@ app.use(cors({
   credentials: true
 }));
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5001;
 const __dirname = path.resolve();
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
 
